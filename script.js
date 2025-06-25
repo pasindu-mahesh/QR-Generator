050386c93398f337bfebcdbe791ef77a57b7c55f
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-06-25 17:10:12 +0530

    feat: add valid url logic

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const form = document.getElementById("genarate-form");
 const qr = document.getElementById("qrcode");
 
 
-
 const onGenarateSubmit = (e) => {
     e.preventDefault();
     const url = document.getElementById("url").value;
     const size = document.getElementById("size").value;
 
     console.log(url, size);
+
+    if(url === ""){
+        alert("please enter a valid URL!");
+    }
+
+    
 
 };  
 
